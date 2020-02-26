d173654f743951db9a0c2abb229eb62970f0867e
Laksh <laksh.dang@gmail.com>
2020-02-26 14:48:24 +0530

    Set up node server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hello World 2')
+// Import Dependencies
+var express = require('express');
+
+// Initialize express app and set port
+var port = process.env.PORT || 3000;
+var app = express();
+
+//Set Express middleware
+app.set('view engine', 'ejs');
+
+//Require routing logic
+require('./app/routes.js')(app);
+//Start server
+app.listen(port);
+console.log("Server running on port: " + port);
